5f26cf18b653dd5a4b343529980e741de6c1b489
Alex Salgado <alexsalgado1@gmail.com>
2021-08-12 20:29:44 -0300

    Primeiro dia de aula

---
+++ OlaMundo.java
@@ -0,0 +1,12 @@
+/**
+ * mat:000 - Alex Salgado
+ */
+public class OlaMundo {
+
+    public static void main(String[] args) {
+
+        System.out.println("Ola Mundo em Java!!!");
+        
+    }
+    
+}
