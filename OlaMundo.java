a761f2c1bfbfeaf24c01652f8abfa1b34d63fa23
Alex Salgado <alexsalgado1@gmail.com>
2021-09-02 19:32:52 -0300

    Update OlaMundo.java

--- OlaMundo.java
+++ OlaMundo.java
@@ -5,7 +5,7 @@ public class OlaMundo {
 
     public static void main(String[] args) {
 
-        System.out.println("Ola Mundo em Java!!!");
+        System.out.println("Olá Mundo em Java!!!");
         
     }
     
